6dcba9148401eb38a34e5cdf6f36b5287f46b995
Matt Pham <matthewminhpham@gmail.com>
2026-09-04 00:28:16 -0700

    Adding Python practice

---
+++ about_m3.py
@@ -0,0 +1,20 @@
+print("=" * 40)
+print("            Student Profile")
+print("=" * 40)
+
+first_name = "Matt"
+last_name = "Pham"
+age = "34"
+city = "Santa Ana"
+career = "Home Care Aide"
+code_reason = "To become a junior ai engineer"
+fun_fact = "I love to produce music"
+
+print(f"Name         {first_name} {last_name}")
+print(f"Age          {age}")
+print(f"City         {city}")
+print(f"Career       {career}")
+print(f"\nWhy I'm learning to code:")
+print(f'"{code_reason}."')
+print(f"\nFun Fact: {fun_fact}.")
+print("=" * 40)
